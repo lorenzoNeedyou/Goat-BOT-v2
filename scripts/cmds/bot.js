eaf0510226a46b15d1efb7f78ef75ac74610c56e
Lorenzo <154640621+lorenzoNeedyou@users.noreply.github.com>
2024-05-08 08:45:30 +0800

    Update bot.js

--- scripts/cmds/bot.js
+++ scripts/cmds/bot.js
@@ -15,6 +15,6 @@ onChat: async function({
 	message,
 	getLang
 }) {
-	if (event.body && event.body.toLowerCase() == "hi") return message.reply("hello Im here, how can i help you?");
+	if (event.body && event.body.toLowerCase() == "bot") return message.reply("hello Im here, how can i help you?");
 }
 };
